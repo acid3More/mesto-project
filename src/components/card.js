1ba6f4a0a62d869ad41755755d775766060259da
acid3More <122834392+acid3More@users.noreply.github.com>
2023-05-08 01:49:11 +0300

    правки после первого ревью

--- src/components/card.js
+++ src/components/card.js
@@ -1,41 +1,9 @@
-import {
-  openPopup,
-  closePopup
-} from "./modal.js";
-
-import {
-  cardList,
-  imagePopup,
-  placeImage,
-  placeText,
-  cardNameInput,
-  cardLinkInput,
-  cardAddSubmitButton,
-  config,
-  cardAddPopup,
-  renderLoading
-} from "./utils.js";
+import { deleteCardUser, openImagePopup } from "./index.js";
 
 import{
-  getUserData,
-  getInitialCards,
-  addCard,
   likeCard,
-  deleteCardData,
   disLikeCard,
 } from './api.js'
-
-import {
-  cleanPopupSpanErrors,
-} from "./validate.js";
-
-// // Открытие попапа карточки
-export function openCardPopup(){
-  cardNameInput.value = '';
-  cardLinkInput.value = '';
-  cleanPopupSpanErrors(cardAddPopup, [cardNameInput, cardLinkInput], cardAddSubmitButton, config);
-  openPopup(cardAddPopup);
-}
 
 // Создаём карточку
 export function createCard (item, userId) {
@@ -52,9 +20,10 @@ export function createCard (item, userId) {
   cardImage.alt = cardName;
   cardElement.dataset.cardId = cardId;
   cardElement.querySelector('.element__name').textContent = cardName;
-  cardElement.querySelector('.element__button').addEventListener('click', likeCardHandler);
   cardDeleteButton.addEventListener('click', deleteCardUser);
-  disableCardDelete(userId, cardOwnerId, cardDeleteButton);
+  cardImage.addEventListener('click', () => openImagePopup(cardImage));
+
+  // Likes
   if(likes.length !== 0) {
     cardElement.querySelector('.element__likes-count').textContent = likes.length;
     const cardLiked = likes.find(like => like._id === userId ? true : false);
@@ -62,110 +31,39 @@ export function createCard (item, userId) {
       cardElement.querySelector('.element__button').classList.toggle('element__button_active');
     };
   }
-  cardImage.addEventListener('click', function() {
-    placeImage.src = cardImage.src;
-    placeImage.alt = cardImage.alt;
-    placeText.textContent = cardImage.alt;
-    openPopup(imagePopup);
-  });
-  return cardElement;
-}
-
-getUserData()
-.then(data => {
-  return data;
-})
-.then(userData => {
-  getInitialCards()
-    .then(cardsData => {
-    cardsData.forEach(function (item) {
-      cardList.append(createCard(item, userData._id));
-    });
-    return cardsData;
-    })
-  .catch((err) => {
-      console.log(err);
-  })
-})
-.catch((err) => {
-  console.log(err);
-})
-
-export function likeCardHandler(evt) {
-  const target = evt.target;
-  const cardElement = target.closest('.element');
-  const cardId = cardElement.dataset.cardId;
-  getInitialCards()
-    .then(cardsData => {
-      let cardObject = cardsData.find(item => {
-        return item._id == cardId;
-      });
-      let cardObjectLikes = cardObject.likes;
-      getUserData()
-        .then(userData => {
-          if (!cardObjectLikes.some(item => {
-            return item.name == userData.name && item.about == userData.about;
-          })) {
-            likeCard(cardObject._id)
-              .then(cardInfo => {
-                target.classList.add('element__button_active');
-                target.closest('.element').querySelector('.element__likes-count').textContent = cardInfo.likes.length;
-              });
-          } else {
-            disLikeCard(cardObject._id)
-            .then(cardInfo => {
-              target.classList.remove('element__button_active');
-              target.closest('.element').querySelector('.element__likes-count').textContent = cardInfo.likes.length;
-            });
+  cardElement.querySelector('.element__button').addEventListener('click', function(evt){
+    const target = evt.target;
+    if (!evt.target.classList.contains('element__button_active')) {
+      likeCard(item._id)
+        .then((card) => {
+          target.classList.add('element__button_active');
+          target.closest('.element').querySelector('.element__likes-count').textContent = card.likes.length;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }
+    else{
+      disLikeCard(item._id)
+        .then((card) => {
+          target.classList.remove('element__button_active');
+          target.closest('.element').querySelector('.element__likes-count').textContent = card.likes.length;
+          if(likes.length <= 0){
+            cardElement.querySelector('.element__likes-count').textContent = '';
           }
         })
         .catch((err) => {
           console.log(err);
-          });
-    })
+        });
+    }
+  })
+  disableCardDelete(userId, cardOwnerId, cardDeleteButton);
+  return cardElement;
 }
 
-export function disableCardDelete(userId, OwnerId, button) {
-  if(userId != OwnerId) {
+function disableCardDelete(userId, ownerId, button) {
+  if(userId != ownerId) {
     button.classList.add('element__remove-btn_inactive');
   }
-}
-
-// Карточка вручную
-export function addCardHandler (evt) {
-  evt.preventDefault();
-  renderLoading(true, cardAddSubmitButton, 'Создать');
-  getUserData()
-    .then(user => {
-    return user._id;
-    })
-    .then(id => {
-      addCard(cardNameInput.value, cardLinkInput.value)
-      .then(res => {
-        cardList.prepend(createCard(res, id));
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    })
-    .catch((err) => {
-    console.log(err);
-    })
-    .finally(() => {
-      renderLoading(false, cardAddSubmitButton, 'Создать');
-      closePopup(cardAddPopup);
-    })
-}
-
-export function deleteCardUser(evt) {
-  const target = evt.target;
-  const card = target.closest('.element');
-  deleteCardData(card.dataset.cardId)
-    .then(() => {
-      card.remove();
-    })
-    .catch((err) => {
-    console.log(err);
-    });
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import '../pages/index.css';
 
-import { getUserData, editUserImage, editUserInfo } from './api';
+import { getUserData, editUserImage, editUserInfo, getInitialCards, addCard, deleteCardData } from './api';
 
 import{
   jobInput,
@@ -21,24 +21,47 @@ import{
   avatarEditInput,
   avatarEditPopup,
   avatarBtnSubmit,
-  renderLoading
+  renderLoading,
+  cardList,
+  cardLinkInput,
+  cardNameInput,
+  cardAddSubmitButton,
+  cardAddPopup,
+  placeImage,
+  placeText,
+  imagePopup
 } from './utils.js';
 
 import{
   enableValidation,
-  toggleButtonState
+  toggleButtonState,
+  cleanPopupSpanErrors
   //
 } from './validate.js';
 
 import{
-  openCardPopup,
-  addCardHandler
+  createCard
 } from './card.js';
 
 import{
   closePopup,
   openPopup,
 } from './modal.js';
+
+Promise.all([getUserData(), getInitialCards()])
+  .then(([user, cards]) => {
+    profileName.textContent = user.name;
+    profileJob.textContent = user.about;
+    avatarImage.src = user.avatar;
+    avatarImage.alt = user.name;
+    const userInfo = user._id;
+    cards.forEach((card) => {
+      cardList.append(createCard(card, userInfo))
+    }) //берем массив карточек из сервера и вставляем в нашу разметку
+  })
+  .catch((err) => {
+    console.log(err) //обработка ошибки
+  })
 
 // Profile
 export function getProfileServerInfo() {
@@ -53,28 +76,20 @@ export function getProfileServerInfo() {
     });
 }
 
-avatarEditContainer.addEventListener('click', () => {
-  avatarEditInput.value = '';
-  openPopup(avatarEditPopup);
-  toggleButtonState([avatarEditInput], avatarBtnSubmit, config)
-});
-
-profileOpenButton.addEventListener('click', () => {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-  openPopup(profilePopup);
-  toggleButtonState([nameInput, jobInput], profileEditSubmitButton, config)
-});
-
 export function changeUserInfoHandler(evt) {
   evt.preventDefault();
   renderLoading(true, profileEditSubmitButton, 'Сохранить');
   editUserInfo(nameInput.value, jobInput.value)
   .then(() => {
     getProfileServerInfo();
-  });
-  renderLoading(false, profileEditSubmitButton, 'Сохранить');
-  closePopup(profilePopup);
+    closePopup(profilePopup);
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  .finally(() => {
+    renderLoading(false, profileEditSubmitButton, 'Сохранить');
+  })
 }
 
 export function changeUserPhotoHandler(evt) {
@@ -84,14 +99,63 @@ export function changeUserPhotoHandler(evt) {
   editUserImage(link)
   .then(() => {
     getProfileServerInfo();
-  });
+    closePopup(avatarEditPopup);
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  .finally(() => {
   renderLoading(false, avatarBtnSubmit, 'Сохранить');
-  closePopup(avatarEditPopup);
+  })
 }
-// ПРОФИЛЬ
+
+// Карточки
+export function openCardPopup(){
+  cardNameInput.value = '';
+  cardLinkInput.value = '';
+  cleanPopupSpanErrors(cardAddPopup, [cardNameInput, cardLinkInput], cardAddSubmitButton, config);
+  openPopup(cardAddPopup);
+}
+
+export function addCardHandler (evt) {
+  evt.preventDefault();
+  renderLoading(true, cardAddSubmitButton, 'Создать');
+  addCard(cardNameInput.value, cardLinkInput.value)
+    .then((card) => {
+      cardNameInput.value = card.name;
+      cardLinkInput.value = card.link
+      cardList.prepend(createCard(card, card.owner._id));
+      evt.target.reset();
+      closePopup(cardAddPopup);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    .finally(() => {
+      renderLoading(false, cardAddSubmitButton, 'Создать');
+    })
+}
+
+export function deleteCardUser(evt) {
+  const target = evt.target;
+  const card = target.closest('.element');
+  deleteCardData(card.dataset.cardId)
+    .then(() => {
+      card.remove();
+    })
+    .catch((err) => {
+    console.log(err);
+    });
+}
+
+export function openImagePopup(card) {
+  placeImage.src = card.src;
+  placeImage.alt = card.alt;
+  placeText.textContent = card.alt;
+  openPopup(imagePopup);
+};
 
 // вызов функций
-getProfileServerInfo();
 enableValidation(config);
 
 // Слушатели
@@ -104,8 +168,21 @@ popups.forEach(popup =>
     closePopup(popup)
   }
   }));
+
 profileFormElement.addEventListener('submit', changeUserInfoHandler);
 avatarEditForm.addEventListener('submit', changeUserPhotoHandler);
-cardForm.addEventListener('submit', addCardHandler)
-cardAddButton.addEventListener('click', openCardPopup)
+cardForm.addEventListener('submit', addCardHandler);
+cardAddButton.addEventListener('click', openCardPopup);
 
+avatarEditContainer.addEventListener('click', () => {
+  avatarEditInput.value = '';
+  openPopup(avatarEditPopup);
+  toggleButtonState([avatarEditInput], avatarBtnSubmit, config)
+});
+
+profileOpenButton.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  openPopup(profilePopup);
+  toggleButtonState([nameInput, jobInput], profileEditSubmitButton, config)
+});
